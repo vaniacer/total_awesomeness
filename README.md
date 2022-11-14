8b8cd22bbdd570b3cea4ea09f3e76bcb1fde8bd0
vaniacer <ie.marov@gmail.com>
2022-11-14 23:18:53 +0300

    Awesome!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # total_awesomeness
 Awesome list creator
+
+Script will create super awesome list of awesome list of awesomeness!)
+Just give it your github token as first arg.
---
+++ awesome.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+token=$1               # your github token
+awesome=${2:-awesome}  # repo type/name to search
+
+die(){ echo $1; exit 1; }
+
+[[ $token   ]] || die "Github token needed!"
+[[ $awesome ]] || die "Set repo type/name to search!"
+
+for i in {1..10}; {
+    curl -s -H "Authorization: Bearer $token" \
+         -H "Accept: application/vnd.github+json" \
+         "https://api.github.com/search/repositories?q=$awesome&sort=stars&order=desc&per_page=100&page=$i" | \
+    jq -r '.items | .[] | select(.description|test("list")) | "<a href=\""+.html_url+"\">"+.name+"</a> "+.description'
+}
