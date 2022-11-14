9e6eadf345474b1e9d9039e5927e881e2a701c51
vaniacer <ie.marov@gmail.com>
2022-11-14 23:23:13 +0300

    Awesome!

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # total_awesomeness
-Awesome list creator
 
 Script will create super awesome list of awesome list of awesomeness!)
 Just give it your github token as first arg.
--- awesome.sh
+++ awesome.sh
@@ -12,5 +12,5 @@ for i in {1..10}; {
     curl -s -H "Authorization: Bearer $token" \
          -H "Accept: application/vnd.github+json" \
          "https://api.github.com/search/repositories?q=$awesome&sort=stars&order=desc&per_page=100&page=$i" | \
-    jq -r '.items | .[] | select(.description|test("list")) | "<a href=\""+.html_url+"\">"+.name+"</a> "+.description'
+    jq -r '.items | .[] | select(.description|test("list")) | "<a href=\""+.html_url+"\">"+.name+"</a> "+.description+"<br>"'
 }
